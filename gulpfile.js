1c45e8c3b7cddd2731445f02fec7a2abde0645f5
Alan Chandler <alan.chandler@intrepidlearning.com>
2015-11-13 10:31:43 -0800

    Big update, lots of style changes and functionality add

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,14 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var minifycss = require('gulp-minify-css');
+var uglify = require('gulp-uglify');
 var nodemon = require('gulp-nodemon');
 var jshint = require('gulp-jshint');
+var rename = require('gulp-rename');
+var concat = require('gulp-concat');
+var path = require('path');
+var fs = require('fs');
 
 gulp.task('sass', function () {
   gulp.src('./public/sass/**/*.scss')
@@ -11,24 +17,39 @@ gulp.task('sass', function () {
             browsers: ['last 2 versions'],
             cascade: false
         }))
-    .pipe(gulp.dest('./public/css'));
+    .pipe(gulp.dest('./public/css'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(minifycss())
+    .pipe(gulp.dest('./public/dist/css'));
+});
+
+gulp.task('scripts', function() {
+  return gulp.src('./public/js/**/*.js')
+    .pipe(jshint('.jshintrc'))
+    .pipe(jshint.reporter('default'))
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('./public/dist/js'))
+    .pipe(rename({suffix: '.min'}))
+    .pipe(uglify())
+    .pipe(gulp.dest('./public/dist/js'));
+});
+
+gulp.task('sass:watch', function () {
+  gulp.watch('public/sass/**/*.scss', ['sass']);
 });
 
 gulp.task('develop', function () {
   nodemon({ script: 'index.js',
             ext: 'html js css',
-            ignore: ['ignored.js'],
-            tasks: ['sass:watch ']
-          })
-    .on('restart', function () {
-      console.log('restarted!');
-    });
-});
-
-gulp.task('watch', function() {
-  gulp.watch('public/sass/style.css', ['styles']);
-});
-
-gulp.task('sass:watch', function () {
-  gulp.watch('public/sass/**/*.scss', ['sass']);
+            env: {'NODE_ENV': 'development'},
+            tasks: function(changedFiles) {
+              var tasks = [];
+              changedFiles.forEach(function(file) {
+                if(path.extname(file) === '.scss' && !~tasks.indexOf('sass:watch')) {
+                  tasks.push('sass:watch');
+                }
+              });
+              return tasks;
+            }
+          });
 });
