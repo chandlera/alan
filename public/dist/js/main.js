617007921faed0a7d6ae3c40b6f3832e66720bd6
Alan Chandler <chandlera718@gmail.com>
2016-05-11 09:42:46 -0700

    Add active state to nav menu

--- public/dist/js/main.js
+++ public/dist/js/main.js
@@ -13,39 +13,18 @@ var menuClick = function menuClick() {
     };
 };
 
-var svgCode = function svgCode() {
-    // SVG color wheel code
-    var $ = window.jQuery;
-    var svgContainer = document.getElementById('svg');
-    var rand = Math.floor(Math.random() * (50 - 1)) + 1;
-    for (var i = 1; i < 6; i++) {
-        var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-        svg.setAttribute('viewbox', '-50 -50 100 100');
-        for (var y = 1; y < 6; y++) {
-            var circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-            var randColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
-            circle.setAttribute('fill', randColor);
-            circle.setAttribute('cx', 50);
-            circle.setAttribute('cy', 50);
-            circle.setAttribute('r', 60 - y * 10);
+var activeMenu = function activeMenu() {
+    var menu = document.getElementsByClassName('nav-menu')[0];
+    var activeMenuItem = menu.dataset.active;
+    var menuItems = Array.prototype.slice.call(document.getElementsByClassName('nav__item'));
 
-            svg.appendChild(circle);
-        }
-        svgContainer.appendChild(svg);
-    }
-
-    $.getJSON('https://crossorigin.me/http://www.colourlovers.com/api/palettes/top?format=json&numResults=5&resultOffset=' + rand, function (data) {
-        $('#svg svg').each(function (x) {
-            $(undefined).find('circle').each(function (y) {
-                if (data[x].colors) {
-                    $(undefined).attr('fill', '#' + data[x].colors[y]);
-                }
-            });
-        });
+    menuItems.forEach(function (item) {
+        item.childNodes[0].textContent === activeMenuItem ? item.className = item.className + ' active' : item.className = item.className.replace('active', '');
     });
 };
 
 document.addEventListener('DOMContentLoaded', function () {
     menuClick();
-    svgCode();
+    // svgCode();
+    activeMenu();
 });
--- public/src/js/main.js
+++ public/src/js/main.js
@@ -9,43 +9,19 @@ let menuClick = () => {
     };
 };
 
-let svgCode = () => {
-    // SVG color wheel code
-    let $ = window.jQuery;
-    let svgContainer = document.getElementById('svg');
-    if(svgContainer) {
-      let rand = Math.floor(Math.random() * (50 - 1)) + 1;
-      for(var i = 1; i < 6; i++) {
-          let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
-          svg.setAttribute('viewbox', '-50 -50 100 100');
-          for(var y = 1; y < 6; y++) {
-              let circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
-              let randColor = '#'+Math.floor(Math.random()*16777215).toString(16);
-              circle.setAttribute('fill', randColor);
-              circle.setAttribute('cx', 50);
-              circle.setAttribute('cy', 50);
-              circle.setAttribute('r', 60-(y*10));
+let activeMenu = () => {
+  const menu = document.getElementsByClassName('nav-menu')[0];
+  const activeMenuItem = menu.dataset.active;
+  const menuItems = Array.prototype.slice.call(document.getElementsByClassName('nav__item'));
 
-              svg.appendChild(circle);
-          }
-          svgContainer.appendChild(svg);
-      }
-
-      $.getJSON('https://crossorigin.me/http://www.colourlovers.com/api/palettes/top?format=json&numResults=5&resultOffset=' + rand, (data) => {
-          $('#svg svg').each((x) => {
-            $(this)
-              .find('circle')
-              .each((y) => {
-                  if(data[x].colors) {
-                      $(this).attr('fill', '#' + data[x].colors[y]);
-                  }
-              });
-          });
-      });
-    }
+  menuItems.forEach(function(item) {
+    item.childNodes[0].textContent === activeMenuItem ? item.className = item.className + ' active' : item.className = item.className.replace('active', '');
+  });
 };
+
 
 document.addEventListener('DOMContentLoaded', () => {
     menuClick();
-    svgCode();
+    // svgCode();
+    activeMenu();
 });
