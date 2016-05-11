c7f18bd4d38f94fa83b4464e66579bca146ed1a1
Alan Chandler <chandlera718@gmail.com>
2016-05-11 09:49:40 -0700

    Add blurb to home page

--- public/dist/js/main.js
+++ public/dist/js/main.js
@@ -23,8 +23,42 @@ var activeMenu = function activeMenu() {
     });
 };
 
+var svgCode = function svgCode() {
+    // SVG color wheel code
+    var $ = window.jQuery;
+    var svgContainer = document.getElementById('svg');
+    if (svgContainer) {
+        var rand = Math.floor(Math.random() * (50 - 1)) + 1;
+        for (var i = 1; i < 6; i++) {
+            var svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+            svg.setAttribute('viewbox', '-50 -50 100 100');
+            for (var y = 1; y < 6; y++) {
+                var circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+                var randColor = '#' + Math.floor(Math.random() * 16777215).toString(16);
+                circle.setAttribute('fill', randColor);
+                circle.setAttribute('cx', 50);
+                circle.setAttribute('cy', 50);
+                circle.setAttribute('r', 60 - y * 10);
+
+                svg.appendChild(circle);
+            }
+            svgContainer.appendChild(svg);
+        }
+
+        $.getJSON('https://crossorigin.me/http://www.colourlovers.com/api/palettes/top?format=json&numResults=5&resultOffset=' + rand, function (data) {
+            $('#svg svg').each(function (x) {
+                $(undefined).find('circle').each(function (y) {
+                    if (data[x].colors) {
+                        $(undefined).attr('fill', '#' + data[x].colors[y]);
+                    }
+                });
+            });
+        });
+    }
+};
+
 document.addEventListener('DOMContentLoaded', function () {
     menuClick();
-    // svgCode();
+    svgCode();
     activeMenu();
 });
--- public/src/js/main.js
+++ public/src/js/main.js
@@ -19,9 +19,44 @@ let activeMenu = () => {
   });
 };
 
+let svgCode = () => {
+    // SVG color wheel code
+    let $ = window.jQuery;
+    let svgContainer = document.getElementById('svg');
+    if(svgContainer) {
+      let rand = Math.floor(Math.random() * (50 - 1)) + 1;
+      for(var i = 1; i < 6; i++) {
+          let svg = document.createElementNS('http://www.w3.org/2000/svg', 'svg');
+          svg.setAttribute('viewbox', '-50 -50 100 100');
+          for(var y = 1; y < 6; y++) {
+              let circle = document.createElementNS('http://www.w3.org/2000/svg', 'circle');
+              let randColor = '#'+Math.floor(Math.random()*16777215).toString(16);
+              circle.setAttribute('fill', randColor);
+              circle.setAttribute('cx', 50);
+              circle.setAttribute('cy', 50);
+              circle.setAttribute('r', 60-(y*10));
+
+              svg.appendChild(circle);
+          }
+          svgContainer.appendChild(svg);
+      }
+
+      $.getJSON('https://crossorigin.me/http://www.colourlovers.com/api/palettes/top?format=json&numResults=5&resultOffset=' + rand, (data) => {
+          $('#svg svg').each((x) => {
+            $(this)
+              .find('circle')
+              .each((y) => {
+                  if(data[x].colors) {
+                      $(this).attr('fill', '#' + data[x].colors[y]);
+                  }
+              });
+          });
+      });
+    }
+};
 
 document.addEventListener('DOMContentLoaded', () => {
     menuClick();
-    // svgCode();
+    svgCode();
     activeMenu();
 });
