c5e7e083323b67374d51b39c01e4a497c1c61e3f
Alan Chandler <alchandler@expedia.com>
2018-07-15 21:17:20 -0700

    No longer use express

--- index.js
+++ index.js
@@ -1,19 +1,9 @@
 /* eslint-env node */
 
-const express = require('express');
+const finalhandler = require('finalhandler');
 const http = require('http');
-const errorhandler = require('errorhandler');
-const helmet = require('helmet');
+const serveStatic = require('serve-static');
 const throng = require('throng');
-const compress = require('compression');
-const webpack = require('webpack');
-const webpackDevMiddleware = require('webpack-dev-middleware');
-
-const routes = require('./routes');
-const app = express();
-
-const config = require('./webpack.config.js');
-const compiler = webpack(config);
 
 const WORKERS = process.env.WEB_CONCURRENCY || 1;
 
@@ -23,27 +13,13 @@ throng({
     start: start
 });
 
-function start() {
-    app.use(helmet());
-    app.set('port', process.env.PORT || 7001);
-    app.set('view engine', 'ejs');
+// Create server
+function start() { 
+    const serve = serveStatic('dist', { index: [ 'index.html' ] });
 
-    app.use(webpackDevMiddleware(compiler, {
-        publicPath: config.output.publicPath,
-        mode: process.env.NODE_ENV
-    }));
-    app.use(compress());
-
-    if (process.env.NODE_ENV === 'development') {
-        app.use(errorhandler());
-    }
-
-    app.get('/', routes.index);
-    app.get('/projects', routes.projects);
-    app.get('/contact', routes.contact);
-    app.get('/.well-known/acme-challenge/2cR2xlfGjq7HPOeqjRiWmekD49JKYazWDX-tzCp-NvU', function (req, res) {
-        res.send('2cR2xlfGjq7HPOeqjRiWmekD49JKYazWDX-tzCp-NvU.cowSSx6eB5gl2OcF18rP9k_bx0-NdNcRZNnxkIrSZb4');
+    const server = http.createServer(function onRequest (req, res) {
+        serve(req, res, finalhandler(req, res));
     });
 
-    http.createServer(app).listen(app.get('port'), function () { });
+    server.listen(process.env.PORT || 7001);
 }
--- routes/index.js
+++
@@ -1,9 +0,0 @@
-exports.index = function (req, res) {
-  res.render('index', { title: 'Home' });
-};
-exports.projects = function (req, res) {
-  res.render('projects', { title: 'Projects' });
-};
-exports.contact = function (req, res) {
-  res.render('contact', { title: 'Contact' });
-};
