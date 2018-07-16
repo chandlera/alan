cd979bbda535902d2287535e9401317aed2ec3d9
Alan Chandler <alchandler@expedia.com>
2018-07-16 10:43:17 -0700

    Add eslint

--- src/js/main.js
+++ src/js/main.js
@@ -8,20 +8,28 @@ import '../images/seattle.png';
 import '../images/wizards.svg';
 
 document.addEventListener('DOMContentLoaded', () => {
-    if ('serviceWorker' in navigator) {
-        window.addEventListener('load', () => {
-            navigator.serviceWorker.register('/service-worker.js').then(registration => {
-                console.log('SW registered: ', registration);
-            }).catch(registrationError => {
-                console.log('SW registration failed: ', registrationError);
-            });
+  if ('serviceWorker' in navigator) {
+    window.addEventListener('load', () => {
+      navigator.serviceWorker
+        .register('/service-worker.js')
+        .then(registration => {
+          console.log('SW registered: ', registration);
+        })
+        .catch(registrationError => {
+          console.log('SW registration failed: ', registrationError);
         });
-    }
+    });
+  }
 
-    [ ...document.querySelectorAll('.nav__item') ].forEach((menuItem) => menuItem.classList.toggle('active', menuItem.querySelector('a').pathname === document.location.pathname));
-    document.querySelector('[data-action=menu-click]').onclick = () => {
-        const menu = document.querySelector('.side-menu');
-        menu.classList.toggle('active');
-        return false;
-    };
+  [...document.querySelectorAll('.nav__item')].forEach(menuItem =>
+    menuItem.classList.toggle(
+      'active',
+      menuItem.querySelector('a').pathname === document.location.pathname
+    )
+  );
+  document.querySelector('[data-action=menu-click]').onclick = () => {
+    const menu = document.querySelector('.side-menu');
+    menu.classList.toggle('active');
+    return false;
+  };
 });
