89d1bbe89e9e82e7f52d376ce8eac05a4bec8ff8
Alan Chandler <alchandler@expedia.com>
2022-03-19 16:02:04 -0700

    feat: initial 11ty migration

---
+++ .eleventy.js
@@ -0,0 +1,53 @@
+const slugify = require('slugify');
+const HtmlMin = require('html-minifier');
+const ErrorOverlay = require('eleventy-plugin-error-overlay');
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.setTemplateFormats(['md']);
+  eleventyConfig.addPlugin(ErrorOverlay);
+
+//   eleventyConfig.addWatchTarget("./src/_tmp/style.css");
+//   eleventyConfig.addPassthroughCopy("./src/images");
+//   eleventyConfig.addPassthroughCopy({ "./src/_tmp/style.css": "./css/style.css" });
+
+  eleventyConfig.addTransform('htmlmin', (content, outputPath) => {
+    if (outputPath.endsWith('.html')) {
+      let minified = HtmlMin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true,
+      });
+      return minified;
+    }
+    return content;
+  });
+
+  eleventyConfig.addShortcode("version", function () {
+    return String(Date.now());
+  });
+
+  let markdownIt = require("markdown-it");
+  let options = {
+    html: true,
+    breaks: true,
+    linkify: true
+  };
+  const MarkDownIt = new markdownIt(options);
+  
+  eleventyConfig.addFilter('markIt', (content) => {
+    return MarkDownIt.render(content);
+  });
+
+  eleventyConfig.addFilter('stringify', (content) => {
+    return JSON.stringify(content, null, 2);
+  });
+
+  return {
+    dir: {
+      input: "src",
+      output: "public",
+      includes: '_templates',
+      data: '_data',
+    }
+  };
+};
